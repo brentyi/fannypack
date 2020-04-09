dd6848c43d1f0577e61ae181739e50fe21919418
Brent Yi <yibrenth@gmail.com>
2020-04-09 15:35:40 -0700

    Add squeeze test

---
+++ tests/utils/test_squeeze.py
@@ -0,0 +1,22 @@
+import pytest
+
+import numpy as np
+import fannypack
+
+
+def test_squeeze_simple():
+    X = np.zeros((10, 1, 5, 1, 10))
+    X = fannypack.utils.squeeze(X)
+    assert X.shape == (10, 5, 10)
+
+
+def test_squeeze_axis():
+    X = np.zeros((10, 1, 5, 1, 10))
+    X = fannypack.utils.squeeze(X, axis=1)
+    assert X.shape == (10, 5, 1, 10)
+
+
+def test_squeeze_bad_axis():
+    X = np.zeros((10, 1, 5, 1, 10))
+    with pytest.raises(AssertionError):
+        X = fannypack.utils.squeeze(X, axis=0)
