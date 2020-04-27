209864fc8f31ef4ce83024bdd32179f3616a8436
Brent Yi <yibrenth@gmail.com>
2020-04-27 03:55:52 -0700

    Release v0.0.3 (type hinting)

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fannypack",
-    version="0.0.3-pre",
+    version="0.0.3",
     description="PyTorch utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
