32222ca9655e1f70b1980f832b452052a3ef6857
Brent Yi <yibrenth@gmail.com>
2020-04-29 07:52:39 -0700

    Revert SliceWrapper changes

--- fannypack/utils/_slice_wrapper.py
+++ fannypack/utils/_slice_wrapper.py
@@ -1,8 +1,6 @@
 # TODO: this class has grown somewhat organically and is pretty messy; could use a more
 # intentional rewrite
 
-from typing import Any, Dict, Iterable, List, Tuple, Union, cast
-
 import numpy as np
 import torch
 
@@ -18,23 +16,15 @@ class SliceWrapper:
     arrays, torch tensors, etc.
     """
 
-    def __init__(
-        self,
-        data: Union[
-            np.ndarray,
-            torch.Tensor,
-            Iterable,
-            Dict[Any, Union[np.ndarray, torch.Tensor, Iterable]],
-        ],
-    ) -> None:
+    def __init__(self, data):
         self._data = data
-        self._type: type = type(data)
+        self._type = type(data)
 
         # Sanity checks
         if type(data) == dict:
             # Every value in the dict should have the same length
             length = None
-            for value in cast(Dict, data).values():
+            for value in data.values():
                 assert type(value) in _valid_iterables
                 l = len(value)
                 if length is None:
@@ -45,29 +35,29 @@ class SliceWrapper:
             # Non-dictionary inputs
             assert type(data) in _valid_iterables, "Invalid datatype!"
 
-    def __getitem__(self, index: int) -> Union[np.ndarray, torch.Tensor, Iterable]:
+    def __getitem__(self, index):
         if self._type == dict:
             # Check that the index is sane
             assert type(index) in (int, slice, tuple)
-            if type(index) == int and cast(int, index) >= len(self):
+            if type(index) == int and index >= len(self):
                 # For use as a standard Python iterator
                 raise IndexError
 
             output = {}
-            for key, value in cast(Dict, self._data).items():
+            for key, value in self._data.items():
                 output[key] = value[index]
             return output
         elif self._type in _valid_iterables:
-            return cast(Dict, self._data)[index]
+            return self._data[index]
         else:
             assert False, "Invalid operation!"
 
-    def __len__(self) -> int:
+    def __len__(self):
         return self.shape[0]
 
-    def append(self, other: Any) -> None:
+    def append(self, other):
         if self._type == dict:
-            self._data = cast(dict, self._data)
+            self._data = self._data
             assert type(other) == dict
             for key, value in other.items():
                 if key in self._data.keys():
@@ -77,62 +67,52 @@ class SliceWrapper:
                         self._data[key] = np.append(self._data[key], value)
                     # Handle standard Python lists
                     elif type(self._data[key]) == list:
-                        cast(Dict, self._data)[key].append(value)
+                        self._data[key].append(value)
                 else:
                     self._data[key] = [value]
-        elif self._type == list:
-            cast(List, self._data).append(other)
+        elif self._type in _mutable_iterables:
+            self._data.append(other)
         else:
             assert False, "Invalid operation!"
 
-    def extend(
-        self,
-        other: Union[
-            np.ndarray,
-            torch.Tensor,
-            Iterable,
-            Dict[Any, Union[np.ndarray, torch.Tensor, Iterable]],
-        ],
-    ):
+    def extend(self, other):
         if self._type == dict:
             assert type(other) == dict
-            for key, value in cast(Dict, other).items():
-                if key in cast(Dict, self._data).keys():
+            for key, value in other.items():
+                if key in self._data.keys():
                     # TODO: add support for torch tensors
                     # Handle numpy arrays (inefficient)
-                    if type(cast(Dict, self._data)[key]) == np.ndarray:
-                        cast(Dict, self._data)[key] = np.concatenate(
-                            (cast(Dict, self._data)[key], value), axis=0
+                    if type(self._data[key]) == np.ndarray:
+                        self._data[key] = np.concatenate(
+                            (self._data[key], value), axis=0
                         )
                     # Handle standard Python lists
                     else:
-                        cast(Dict, self._data)[key].extend(value)
+                        self._data[key].extend(value)
                 else:
-                    cast(Dict, self._data)[key] = value
-        elif self._type == list:
-            cast(List, self._data).extend(other)
+                    self._data[key] = value
+        elif self._type in _mutable_iterables:
+            self._data.extend(other)
         else:
-            assert False, "Unsupported operation!"
+            assert False, "Invalid operation!"
 
-    def convert_to_numpy(self) -> None:
+    def convert_to_numpy(self):
         if self._type == dict:
             # Convert elements in dictionary to numpy
-            for key, value in cast(Dict, self._data).items():
-                cast(Dict, self._data)[key] = np.asarray(value)
+            for key, value in self._data.items():
+                self._data[key] = np.asarray(value)
         else:
             # Convert contents (list, tuple, etc) to numpy
             self._data = np.asarray(self._data)
 
     @property
-    def shape(self) -> Tuple:
+    def shape(self):
         if self._type == dict:
-            output: Tuple
-            first = True
-            for value in cast(Dict, self._data).values():
+            output = None
+            for value in self._data.values():
                 shape = self._shape_helper(value)
-                if first:
+                if output == None:
                     output = shape
-                    first = False
                 else:
                     for i in range(min(len(output), len(shape))):
                         if output[i] != shape[i]:
@@ -143,7 +123,7 @@ class SliceWrapper:
             return self._shape_helper(self._data)
 
     @staticmethod
-    def _shape_helper(data) -> Tuple:
+    def _shape_helper(data):
         if type(data) in (torch.Tensor, np.ndarray):
             # Return full shape
             return data.shape
@@ -154,12 +134,5 @@ class SliceWrapper:
             assert False, "Invalid operation!"
 
     @property
-    def data(
-        self,
-    ) -> Union[
-        np.ndarray,
-        torch.Tensor,
-        Iterable,
-        Dict[Any, Union[np.ndarray, torch.Tensor, Iterable]],
-    ]:
+    def data(self):
         return self._data
