83fc24020dc9bf16b78287a5038b7874baacfd24
Brent Yi <yibrenth@gmail.com>
2020-06-15 22:38:59 -0700

    Add `get_git_commit_hash()` + test

--- fannypack/utils/__init__.py
+++ fannypack/utils/__init__.py
@@ -2,6 +2,7 @@ from ..data import _trajectories_file
 from ._buddy import Buddy
 from ._conversions import to_device, to_numpy, to_torch
 from ._deprecation import deprecation_wrapper, new_name_wrapper
+from ._git import get_git_commit_hash
 from ._module_freezing import freeze_module, unfreeze_module
 from ._pdb_safety_net import pdb_safety_net
 from ._slice_wrapper import SliceWrapper
---
+++ tests/utils/test_git.py
@@ -0,0 +1,7 @@
+import fannypack
+
+
+def test_get_git_commit_hash():
+    """Checks that we can read this file's commit hash.
+    """
+    assert len(fannypack.utils.get_git_commit_hash(__file__)) == 40
