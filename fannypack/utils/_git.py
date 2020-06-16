1771791d7eb844294226a83577632f742e8d84ec
Brent Yi <yibrenth@gmail.com>
2020-06-15 22:47:55 -0700

    `get_git_commit_hash()` default argument fix

--- fannypack/utils/_git.py
+++ fannypack/utils/_git.py
@@ -2,11 +2,11 @@ import os
 import subprocess
 
 
-def get_git_commit_hash(path: str = ".") -> str:
+def get_git_commit_hash(path: str = "./") -> str:
     """Returns the current Git commit hash.
 
     Args:
-        path (str, optional): Path to check. Defaults to '.'.
+        path (str, optional): Path to check. Defaults to './'.
     """
     return (
         subprocess.check_output(["git", "rev-parse", "HEAD"], cwd=os.path.dirname(path))
