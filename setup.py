cd4c0d6c4838de4d6c3a5c1c009158e225e1a87c
Brent Yi <yibrenth@gmail.com>
2020-06-22 01:14:20 -0700

    Release v0.0.13

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fannypack",
-    version="0.0.12",
+    version="0.0.13",
     description="PyTorch utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
