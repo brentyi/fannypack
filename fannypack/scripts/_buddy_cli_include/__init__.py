ed2e8c2b2e508463f0739149586f886f6e57079c
Brent Yi <yibrenth@gmail.com>
2020-08-02 06:22:46 -0700

    Mypy fix

--- fannypack/scripts/_buddy_cli_include/__init__.py
+++ fannypack/scripts/_buddy_cli_include/__init__.py
@@ -1,13 +1,6 @@
 # Register subcommands
-from . import _subcommand_delete as _
-from . import _subcommand_info as _
-from . import _subcommand_list as _
-from . import _subcommand_rename as _
+from . import _subcommand_delete, _subcommand_info, _subcommand_list, _subcommand_rename
 
 # Expose important stuff for CLI script
 from ._subcommand import Subcommand, subcommand_registry
 from ._utils import BuddyPaths
-
-# Erase `_`
-_ = None
-del _
