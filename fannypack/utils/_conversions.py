403edb51507775d476f3b0379c7ad7db9f34760d
Brent Yi <yibrenth@gmail.com>
2020-08-14 02:17:03 -0700

    Remove support for recursive dataclass conversions :shrug:

--- fannypack/utils/_conversions.py
+++ fannypack/utils/_conversions.py
@@ -1,26 +1,24 @@
-import dataclasses
 import functools
-from typing import Callable, Dict, List, Tuple, TypeVar, Union, cast, overload
+from typing import Callable, Dict, List, Tuple, Type, TypeVar, Union, cast, overload
 
 import numpy as np
 import torch
 
-# Type aliases, variables
 Container = TypeVar("Container", Dict, List, Tuple)
-InputType = TypeVar("InputType", np.ndarray, torch.Tensor)
-OutputType = TypeVar("OutputType", np.ndarray, torch.Tensor)
+_InputType = TypeVar("_InputType", np.ndarray, torch.Tensor)
+_OutputType = TypeVar("_OutputType", np.ndarray, torch.Tensor)
 
 
 @overload
 def _convert_recursive(
-    x: InputType, convert: Callable[[InputType], OutputType], input_type: type,
-) -> OutputType:
+    x: _InputType, convert: Callable[[_InputType], _OutputType], input_type: Type,
+) -> _OutputType:
     ...
 
 
 @overload
 def _convert_recursive(
-    x: Container, convert: Callable[[InputType], OutputType], input_type: type,
+    x: Container, convert: Callable[[_InputType], _OutputType], input_type: Type,
 ) -> Container:
     ...
 
@@ -32,7 +30,7 @@ def _convert_recursive(x, convert, input_type):
 
     # Conversion base case
     if isinstance(x, input_type):
-        x = cast(InputType, x)
+        x = cast(_InputType, x)
         return convert(x)
 
     # Convert containers: bind arguments to helper function
@@ -43,7 +41,7 @@ def _convert_recursive(x, convert, input_type):
     # Convert dictionaries of values
     if isinstance(x, dict):
         x = cast(dict, x)
-        return {k: convert_recursive(v) for k, v in x.items()}
+        return dict(zip(x.keys(), map(convert_recursive, x.values())))
 
     # Convert lists of values
     if isinstance(x, list):
@@ -54,19 +52,6 @@ def _convert_recursive(x, convert, input_type):
     if isinstance(x, tuple):
         x = cast(tuple, x)
         return tuple(map(convert_recursive, x))
-
-    # Convert dataclass containing values
-    if dataclasses.is_dataclass(x):
-        changes = {}
-        for field in dataclasses.fields(x):
-            value = getattr(x, field.name)
-            try:
-                changes[field.name] = convert_recursive(value)
-            except AssertionError:
-                # For dataclasses, we leave unsupported types alone
-                # May want to rethink this?
-                pass
-        return dataclasses.replace(x, **changes)
 
     # Unsupported input types
     assert False, f"Unsupported datatype {type(x)}!"
@@ -87,18 +72,17 @@ def to_device(x: Container, device: torch.device, detach: bool = False) -> Conta
 def to_device(
     x: Union[Container, torch.Tensor], device: torch.device, detach: bool = False
 ) -> Union[Container, torch.Tensor]:
-    """Move a torch tensor, list of tensors, dict, or dataclass of tensors to a
+    """Move a torch tensor, list, tuple, or dict of tensors to a
     different device. Recursively applied for nested containers.
 
     Args:
-        x: (torch.Tensor, list, tuple, dict, or dataclass) Tensor or container of
-            tensors to move.
+        x (torch.Tensor, list, tuple, or dict): Tensor or container of tensors to move.
         device (torch.device): Target device.
         detach (bool, optional): If set, detaches tensors after moving. Defaults to
             False.
 
     Returns:
-        torch.Tensor, list, tuple, dict, or dataclass: Output, type will mirror input.
+        torch.Tensor, list, tuple, or dict: Output, type will mirror input.
     """
 
     def convert(x):
@@ -126,19 +110,19 @@ def to_torch(
 def to_torch(
     x, device="cpu", convert_doubles_to_floats=True,
 ):
-    """Converts a numpy array, list of numpy arrays, dict, or dataclass of numpy arrays
-    for use in PyTorch. Recursively applied for nested containers.
+    """Converts a NumPy array, list, tuple, or dict of NumPy arrays for use in PyTorch.
+    Recursively applied for nested containers.
 
     Args:
-        x: (np.ndarray, list, tuple, dict, or dataclass) Array or container of arrays to
-            convert to torch tensors.
+        x (np.ndarray, list, tuple, or dict): Array or container of arrays to convert to
+            torch tensors.
         device (torch.device, optional): Torch device to create tensors on. Defaults to
             `"cpu"`.
         convert_doubles_to_floats (bool, optional): If set, converts 64-bit floats to
             32-bit. Defaults to True.
 
     Returns:
-        torch.Tensor, list, tuple, dict, or dataclass: Output, type will mirror input.
+        torch.Tensor, list, tuple, or dict: Output, type will mirror input.
     """
 
     def convert(x: np.ndarray) -> torch.Tensor:
@@ -162,15 +146,15 @@ def to_numpy(x: Container) -> Container:
 
 
 def to_numpy(x):
-    """Converts a tensor, list of tensors, dict, or dataclass of tensors for use in
-    Numpy. Recursively applied for nested containers.
+    """Converts a tensor, list, tuple, or dict of tensors for use in Numpy. Recursively
+    applied for nested containers.
 
     Args:
-        x: (torch.Tensor, list, tuple, dict, or dataclass) Tensor or container of
-            tensors to convert to numpy.
+        x (torch.Tensor, list, tuple, or dict): Tensor or container of tensors to
+            convert to NumPy.
 
     Returns:
-        np.ndarray, list, tuple, dict, or dataclass: Output, type will mirror input.
+        np.ndarray, list, tuple, or dict: Output, type will mirror input.
     """
 
     def convert(x: torch.Tensor) -> np.ndarray:
--- tests/utils/test_conversions.py
+++ tests/utils/test_conversions.py
@@ -1,5 +1,4 @@
-from dataclasses import dataclass
-from typing import Any
+from typing import Any, Dict, Tuple
 
 import numpy as np
 import pytest
@@ -8,59 +7,46 @@ import torch
 import fannypack
 
 
-@dataclass
-class Dataclass:
-    value: Any
-    string: str = "True"
-    boolean: bool = False
-
-
 @pytest.fixture
-def nested_conversion():
-    X = Dataclass(value={"data": ([np.zeros((10, 1, 5, 1, 10))], np.eye(3))})
+def nested_conversion() -> Tuple[Dict, Dict, Dict]:
+    X = {"data": ([np.zeros((10, 1, 5, 1, 10))], np.eye(3))}
     X_torch = fannypack.utils.to_torch(X)
     X_numpy = fannypack.utils.to_numpy(X_torch)
     return X, X_torch, X_numpy
 
 
-def test_dataclass_checks(nested_conversion):
-    """Check for dataclass field issues.
+def test_not_same(nested_conversion: Tuple[Dict, Dict, Dict]):
+    """Make sure we've received three distinct dictionaries.
     """
     X, X_torch, X_numpy = nested_conversion
-
     assert X is not X_torch
     assert X is not X_numpy
 
-    assert X.string == X_torch.string
-    assert X.string == X_numpy.string
-    assert X.boolean == X_torch.boolean
-    assert X.boolean == X_numpy.boolean
 
-
-def test_conversion_shapes(nested_conversion):
+def test_conversion_shapes(nested_conversion: Tuple[Dict, Dict, Dict]):
     """Check numpy <=> torch conversions.
     """
     X, X_torch, X_numpy = nested_conversion
-    assert X_torch.value["data"][0][0].shape == X_numpy.value["data"][0][0].shape
-    assert X_torch.value["data"][1].shape == X_numpy.value["data"][1].shape
+    assert X_torch["data"][0][0].shape == X_numpy["data"][0][0].shape
+    assert X_torch["data"][1].shape == X_numpy["data"][1].shape
 
 
-def test_conversion_types(nested_conversion):
+def test_conversion_types(nested_conversion: Tuple[Dict, Dict, Dict]):
     """Check numpy <=> torch conversions.
     """
     X, X_torch, X_numpy = nested_conversion
-    assert type(X_numpy.value["data"][0][0]) == np.ndarray
-    assert type(X_torch.value["data"][0][0]) == torch.Tensor
-    assert type(X_numpy.value["data"][1]) == np.ndarray
-    assert type(X_torch.value["data"][1]) == torch.Tensor
+    assert type(X_numpy["data"][0][0]) == np.ndarray
+    assert type(X_torch["data"][0][0]) == torch.Tensor
+    assert type(X_numpy["data"][1]) == np.ndarray
+    assert type(X_torch["data"][1]) == torch.Tensor
 
 
-def test_conversion_values(nested_conversion):
+def test_conversion_values(nested_conversion: Tuple[Dict, Dict, Dict]):
     """Check numpy <=> torch conversions.
     """
     X, X_torch, X_numpy = nested_conversion
-    assert np.allclose(X.value["data"][0][0], X_numpy.value["data"][0][0])
-    assert np.allclose(X.value["data"][1], X_numpy.value["data"][1])
+    assert np.allclose(X["data"][0][0], X_numpy["data"][0][0])
+    assert np.allclose(X["data"][1], X_numpy["data"][1])
 
 
 def test_conversion_failure():
