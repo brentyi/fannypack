2888aa5d969824ac1e1a528264674ece3f4703f9
Brent Yi <yibrenth@gmail.com>
2021-11-09 17:10:42 -0800

    Update setup.py

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as fh:
 
 setup(
     name="fannypack",
-    version="0.0.23",
+    version="0.0.25",
     description="PyTorch utilities",
     long_description=long_description,
     long_description_content_type="text/markdown",
